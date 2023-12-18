87fca228c11857a38a00ed8bf666a6d99a6b7372
Vidi Shaweddy <vidi.shaweddy@readytech.io>
2023-12-19 01:29:29 +1030

    Use get method

--- lib/openid_connect/client.rb
+++ lib/openid_connect/client.rb
@@ -17,6 +17,22 @@ module OpenIDConnect
       absolute_uri_for userinfo_endpoint
     end
 
+    def access_token!(*args)
+      headers, params, http_client, options = authenticated_context_from(*args)
+      params[:scope] = Array(options.delete(:scope)).join(' ') if options[:scope].present?
+      params.merge! @grant.as_json
+      params.merge! options
+      handle_response do
+        http_client.get(
+          absolute_uri_for(token_endpoint),
+          Util.compact_hash(params),
+          headers
+        ) do |req|
+          yield req if block_given?
+        end
+      end
+    end
+
     private
 
     def setup_required_scope(scopes)
