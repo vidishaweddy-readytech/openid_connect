8793173307c0a369a4e4e35aa0bb6a0e1fc8f334
Vidi Shaweddy <vidi.shaweddy@readytech.io>
2023-12-19 10:24:42 +1030

    Fix missing method

--- lib/openid_connect/client.rb
+++ lib/openid_connect/client.rb
@@ -25,7 +25,7 @@ module OpenIDConnect
       handle_response do
         http_client.get(
           absolute_uri_for(token_endpoint),
-          ::Rack::Oauth2::Util.compact_hash(params),
+          ::Rack::OAuth2::Util.compact_hash(params),
           headers
         ) do |req|
           yield req if block_given?
